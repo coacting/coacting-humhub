073ee922cb3084b89a438c3162485c1eabccf3d7
Eldon Marks <emarks@version75.com>
2020-05-25 16:58:41 -0400

    Added homepage url config too common.php; restricted languages to en-US

--- protected/config/common.php
+++ protected/config/common.php
@@ -5,11 +5,27 @@
  * @see http://docs.humhub.org/admin-installation-configuration.html
  * @see http://docs.humhub.org/dev-environment.html
  */
+
+  // output: /myproject/index.php
+  $currentPath = $_SERVER['PHP_SELF'];
+  // output: Array ( [dirname] => /myproject [basename] => index.php [extension] => php [filename] => index )
+  $pathInfo = pathinfo($currentPath);
+  // output: localhost
+  $hostName = $_SERVER['HTTP_HOST'];
+  // output: http://
+  $protocol = strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,5))=='https'?'https':'http';
+  // return: http://localhost/myproject/
+  $baseUrl = $protocol.'://'.$hostName.$pathInfo['dirname']."/";
+
 return [
+  'homeUrl' => $baseUrl.'p/home', //add custom homepage
   'components' => [
     'urlManager' => [
       'showScriptName' => false,
       'enablePrettyUrl' => true,
     ],
+  ],
+  'params' => [
+    'allowedLanguages' => ['en-US']
   ]
 ];
